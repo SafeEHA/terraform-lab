3c06faf6c357180ace638d91d6eb09a7df46e5a8
safeeha <eemarabs@gmail.com>
2025-05-02 09:56:28 +0100

    config files

---
+++ commands.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+aws s3 mb s3://terraform-bucket-0205 --region us-west-2
+
+
+aws dynamodb create-table \
+  --table-name terraform-locks \
+  --attribute-definitions AttributeName=LockID,AttributeType=S \
+  --key-schema AttributeName=LockID,KeyType=HASH \
+  --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+  --region us-west-2
+
